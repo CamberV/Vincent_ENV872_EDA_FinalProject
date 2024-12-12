ef61980f1b301faa6e22c206f671d9c9360db4e3
CamberV <cav33@duke.edu>
2024-12-12 18:46:24 -0500

    First attempt at ui and server construction to ensure logic works. Success in running app.

--- server.R
+++ server.R
@@ -1,5 +1,8 @@
 library(shiny)
+library(sf)
 library(ggplot2)
+library(dplyr)
+library(viridis)
 
 server<-function(input, output, session){
   
@@ -12,13 +15,45 @@ server<-function(input, output, session){
     )
   })
   
-  #map visualization
-  output$map_plot<-renderPlot({
-    ggplot()
+  #reactive data selection based on user input
+  selected_data <- reactive({
+    
+    #select which sf data file to visualize based on regional level
+    map_data<-if(input$geo_level=="SR")map_sr_data else map_st_data
+    
+    #selecting the column name based on user input
+    column_name<-paste0(input$geo_level,input$pollutant,input$emission_rate)
+    
+    #re-titling selected data as a new column "data"
+    map_data<-map_data%>%
+      mutate(value =.[[column_name]]) #select column based on previous line
+    map_data
   })
   
-  #bar chart visualization
-  output$bar_chart<-renderPlot({
-    ggplot()
+  #render the map visualization
+  output$map_plot <- renderPlot({
+    map_data <- selected_data()
+    
+    ggplot(data = map_data) +
+      geom_sf(aes(fill = value), color = "black") +
+      scale_fill_viridis_c(option = "C", na.value = "white") +
+      labs(title = "Emission Map", fill = "Value") +
+      theme_minimal()
+  })
+  
+  #render the bar chart visualization
+  output$bar_chart <- renderPlot({
+    map_data <- selected_data()
+    
+    ggplot(data = map_data) +
+      geom_bar(aes(x = ifelse(input$geo_level == "SR", SUBRGN, PSTATABB), y = value),
+               stat = "identity", fill = "steelblue") +
+      labs(
+        title = "Emission Bar Chart",
+        x = ifelse(input$geo_level == "SR", "Subregion", "State"),
+        y = "Value"
+      ) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
   })
 }
--- ui.R
+++ ui.R
@@ -6,27 +6,30 @@ ui<-fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput(
-        inputId="emission_type",
-        label="Select Emission Type:",
-        choices=c("Output Emission Rates", "Input Emission Rates", 
-                    "Non-Baseload Output Emission Rates")
+        inputId="geo_level",
+        label="Select Geographic Level:",
+        choices=c("Subregion"="SR","State"="ST")
+      ),
+      selectInput(
+        inputId="emission_rate",
+        label="Select Emission Rate:",
+        choices=c("Output Emission Rates"="RTA", 
+                    "Input Emission Rates"="RA", 
+                    "Non-Baseload Emissions"="NB")
       ),
       selectInput(
         inputId="pollutant",
         label="Select Pollutant:",
-        choices=c("CO2", "NOX", "SO2", "CH4", "N2O", "CO2eq")
-      ),
-      selectInput(
-        inputId="geo_level",
-        label="Select Geographic Level:",
-        choices=c("eGRID Subregion", "State")
+        choices=c("CO2","NOX","SO2","CH4","N2O","CO2eq"="C2E")
       ),
       textOutput("selection_summary")
     ),
     
     mainPanel(
-      h3("Visualizations"),
+      h3("Map Visualization"),
       plotOutput("map_plot"),
+      
+      h3("Bar Chart"),
       plotOutput("bar_chart")
     )
   )
